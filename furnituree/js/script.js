a8f466a79df5691ef40b3a76e4f8294efe2f4bbf
ksenzo <ksenz.gleb61@gmail.com>
2022-01-06 01:34:01 +0200

    second commit

---
+++ furnituree/js/script.js
@@ -0,0 +1,8 @@
+window.onscroll = function showHeader() {
+   let fixedHeader = document.querySelector('.header');
+   if (window.pageYOffset > 0) {
+      fixedHeader.classList.add('__active');
+   } else {
+      fixedHeader.classList.remove('__active');
+   }
+};
