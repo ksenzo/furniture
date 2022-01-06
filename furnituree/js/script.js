e640791a910af860642e707656a62478bafcc12b
ksenzo <ksenz.gleb61@gmail.com>
2022-01-06 17:35:23 +0200

    third commit

--- furnituree/js/script.js
+++ furnituree/js/script.js
@@ -1,3 +1,21 @@
+let burgerBtn = document.querySelector('.header__menu-burger');
+let closeBtn = document.querySelector('.menu-mobile__close');
+
+if (burgerBtn) {
+   let menuMobile = document.querySelector('.menu-mobile');
+   burgerBtn.onclick = function() {
+      menuMobile.classList.add('__active');
+   };
+}
+
+if (closeBtn) {
+   let menuMobile = document.querySelector('.menu-mobile');
+   closeBtn.onclick = function() {
+      menuMobile.classList.remove('__active');
+   };
+}
+
+
 window.onscroll = function showHeader() {
    let fixedHeader = document.querySelector('.header');
    if (window.pageYOffset > 0) {
