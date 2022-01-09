3398683560e23f755da158c6d04a4c798ebf4f5e
ksenzo <ksenz.gleb61@gmail.com>
2022-01-09 15:31:51 +0200

    adaptive tabs

--- furnituree/js/script.js
+++ furnituree/js/script.js
@@ -1,10 +1,35 @@
 let burgerBtn = document.querySelector('.header__menu-burger');
 let closeBtn = document.querySelector('.menu-mobile__close');
+let body = document.body;
+let showMoreBtn = document.querySelector('.about-us__text-more-btn');
+let materials = document.querySelector('.materials__materials');
+let furniture = document.querySelector('.materials__furniture');
+let rowTab = document.querySelector('.row-tabs');
+
+if(furniture) {
+   furniture.onclick = function() {
+      rowTab.classList.add('__active');
+   };
+}
+
+if(materials) {
+   materials.onclick = function() {
+      rowTab.classList.remove('__active');
+   };
+}
+
+if(showMoreBtn) {
+   let textMore = document.querySelector('.about-us__text-mobile-more');
+   showMoreBtn.onclick = function() {
+      textMore.classList.toggle('__active');
+   };
+}
 
 if (burgerBtn) {
    let menuMobile = document.querySelector('.menu-mobile');
    burgerBtn.onclick = function() {
       menuMobile.classList.add('__active');
+      body.classList.add('__noscroll');
    };
 }
 
@@ -12,6 +37,7 @@ if (closeBtn) {
    let menuMobile = document.querySelector('.menu-mobile');
    closeBtn.onclick = function() {
       menuMobile.classList.remove('__active');
+      body.classList.remove('__noscroll');
    };
 }
 
